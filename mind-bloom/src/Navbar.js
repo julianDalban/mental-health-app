c26e3bf96a84eea109f61d6dfdea26f37570b38a
Julian Dalban <julian.dalban@gmail.com>
2024-11-30 18:29:20 -0500

    Created Navbar and Logo Components
    New navbar + Logo seperated so easy to change in hindsight

--- mind-bloom/src/Navbar.js
+++ mind-bloom/src/Navbar.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Logo from './Logo';
 
 function Navbar() {
   return (
-    <nav className="bg-blue-500 p-4">
+    <nav className="bg-green-500 p-4">
       <div className="container mx-auto">
         <div className="flex justify-between items-center">
-          <div className="text-white font-bold text-xl">My Website</div>
+          <Logo />
           <div className="hidden md:flex space-x-4">
             <a href="#" className="text-white hover:text-gray-300">Home</a>
             <a href="#" className="text-white hover:text-gray-300">About</a>
