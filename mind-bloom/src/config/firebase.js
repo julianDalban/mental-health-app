48c04ef54742fe6fabab35e795effcd54dc71e06
JayBradCoding <138684111+JayBradCoding@users.noreply.github.com>
2024-11-30 01:49:46 -0500

    Setup firebase and basic react outline

---
+++ mind-bloom/src/config/firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: "AIzaSyASKIQ-mEfJ4PomJMtSfJG-5Y9X5WLJ7zY",
+  authDomain: "mindbloom-d66d8.firebaseapp.com",
+  projectId: "mindbloom-d66d8",
+  storageBucket: "mindbloom-d66d8.firebasestorage.app",
+  messagingSenderId: "590102057351",
+  appId: "1:590102057351:web:7f7de3bdee8e22d3928322",
+  measurementId: "G-YT5G66L7FQ"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
