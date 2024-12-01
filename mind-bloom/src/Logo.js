a2143e887e002f2dba4d3aec20da0b6140a44823
Julian Dalban <julian.dalban@gmail.com>
2024-11-30 22:15:37 -0500

    Slight change to logo
    Changed colour to darker green

--- mind-bloom/src/Logo.js
+++ mind-bloom/src/Logo.js
@@ -3,13 +3,13 @@ import brainIcon from './pictures/new_logo_face_brain.png';
 
 function Logo() {
     return (
-        <div className="p-6 max-w-sm mx-auto bg-green rounded-xl shadow-lg flex items-center gap-x-4">
+        <div className="p-6 max-w-sm mx-auto bg-green-600 rounded-xl shadow-lg flex items-center gap-x-4">
             <div className="shrink-0">
-                <img className="size-12" src={brainIcon} alt="Mind Bloom Logo" />
+                <img className="size-12 bg-origin-border" src={brainIcon} alt="Mind Bloom Logo" />
             </div>
             <div>
                 <div className="text-black font-bold text-xl">Mind Bloom</div>
-                <p className="text-slate-500-black ">Access all resources for mental health</p>
+                <p className="text-slate-500-black">Access all resources for mental health</p>
             </div>
         </div>
     );
