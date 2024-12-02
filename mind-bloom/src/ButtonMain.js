cefab95f90d3a0a87fadee365e9e0f419daecec5
Julian Dalban <julian.dalban@gmail.com>
2024-12-01 23:44:40 -0500

    Starting to work on buttons
    Also made slight change to hover effect on logo

---
+++ mind-bloom/src/ButtonMain.js
@@ -0,0 +1,9 @@
+function ButtonMain() {
+  return (
+    <div>
+      <button className="button-main">Get Started</button>
+    </div>
+  );
+}
+
+export default ButtonMain;
--- mind-bloom/src/Logo.js
+++ mind-bloom/src/Logo.js
@@ -3,13 +3,13 @@ import brainIcon from './pictures/new_logo_face_brain.png';
 
 function Logo() {
     return (
-        <div className="p-6 max-w-sm mx-auto bg-green-600 rounded-xl shadow-lg flex items-center gap-x-4">
+        <div className="p-6 max-w-sm mx-auto bg-gradient-to-r from-green-500 from-10% from-emerald-600 rounded-xl shadow-lg flex items-center gap-x-4 hover:from-teal-600 ">
             <div className="shrink-0">
-                <img className="size-12 bg-origin-border" src={brainIcon} alt="Mind Bloom Logo" />
+                <img className="size-12 git bg-origin-border" src={brainIcon} alt="Mind Bloom Logo" />
             </div>
             <div>
-                <div className="text-black font-bold text-xl">Mind Bloom</div>
-                <p className="text-slate-500-black">Access all resources for mental health</p>
+                <div className="text-black font-bold text-xl font-serif">Mind Bloom</div>
+                <p className="text-slate-500-black font-serif">Access all resources for mental health</p>
             </div>
         </div>
     );
