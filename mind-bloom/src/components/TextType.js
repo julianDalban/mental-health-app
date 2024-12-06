6eb75316e6439691a70faa94d66d056ef5e84cde
Julian Dalban <julian.dalban@gmail.com>
2024-12-06 12:19:19 -0500

    Finished the TextType Component and slight style changes to the main buttons.

--- mind-bloom/src/components/TextType.js
+++ mind-bloom/src/components/TextType.js
@@ -3,23 +3,24 @@ import { TypeAnimation } from "react-type-animation";
 
 const TextType = ({text, author}) => {
     return(
-        <div className="flex justify-between items-center relative bg-blue-500 h-[9rem] w-[20rem]">
-            <div className="absolute bottom-0 bg-red-300 h-[8rem] w-[18rem] p-3 line-clamp-4 hover:line-clamp-none">
+        <div className="border-2 border-emerald-500 outline-offset-2 rounded-md flex justify-between items-center relative h-[9rem] w-[20rem]">
+            <div className="absolute bottom-0 h-[8rem] w-[18rem] p-3 line-clamp-4">
                 <TypeAnimation
                     sequence={[
                         //Text goes here
-                        text,
-                        4000, // delay
                         ' - ',
-                        500
+                        1000,
+                        text,
+                        7000 // delay
                     ]}
                     deletionSpeed={50}
                     speed={30}
-                    style={{fontSize: '1.125rem', display: 'flex', justifyContent: 'left', alignItems: 'left', fontFamily: 'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace'}}
+                    cursor={false}
+                    style={{fontSize: '1rem', display: 'flex', justifyContent: 'left', alignItems: 'left', fontFamily: 'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace'}}
                     repeat={Infinity}
                 />
             </div>
-            <div className="bg-red-700 absolute top-0 left-0 text-base font-serif subpixel-antialiased italic font-medium"> - {author} </div>
+            <div className="absolute top-0 left-0 text-sm font-serif subpixel-antialiased italic font-medium leading-6"> {author} </div>
         </div>
     );
 };
