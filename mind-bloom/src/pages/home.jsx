f294d0ff5c0c1618c1f4ff2a49f06e14c5c79a09
Julian Dalban <julian.dalban@gmail.com>
2024-12-12 02:30:45 -0500

    Updated home page to use component button

--- mind-bloom/src/pages/home.jsx
+++ mind-bloom/src/pages/home.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import brainIcon from "./../pictures/new_logo_face_brain.png";
 import Green from './../pictures/Green.jpg';
+import { useNavigate } from 'react-router-dom';
+import ButtonMain from '../components/ButtonMain';
 
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="relative flex flex-col items-center justify-center min-h-screen bg-cover bg-center py-12" style={{ backgroundImage: `url(${Green})` }}>
       <div className="relative z-10 bg-white bg-opacity-90 p-8 rounded-lg shadow-lg max-w-2xl w-full">
@@ -19,11 +23,6 @@ const Home = () => {
             <li>Community support and forums</li>
             <li>Professional counseling and therapy sessions</li>
           </ul>
-          <Link to="/services" className="visible border-1 font-serif border-double border-slate-100 px-5 py-1 rounded-full shadow-lg transition 
-      ease-in-out delay-150 bg-emerald-500 hover:-translate-y-1 hover:scale-110 
-      hover:bg-teal-500 duration-300 mb-8">
-            Explore Our Services
-          </Link>
         </div>
         <div className="mb-8">
           <h2 className="text-2xl font-semibold mb-2">You may ask, why green?</h2>
@@ -41,11 +40,7 @@ const Home = () => {
           </div>
         </div>
         <div className="flex justify-center">
-          <Link to="/services" className="visible border-1 font-serif border-double border-slate-100 px-5 py-1 rounded-full shadow-lg transition 
-      ease-in-out delay-150 bg-emerald-500 hover:-translate-y-1 hover:scale-110 
-      hover:bg-teal-500 duration-300">
-            Get Started
-          </Link>
+        <ButtonMain text={'Get Started'} onClick={() => navigate('/services')}/>
         </div>
       </div>
     </div>
